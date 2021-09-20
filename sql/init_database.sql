56cebe60e2ad8878b7ee5b5de18441be44c14282
paffel2 <paffel2@gmail.com>
2021-09-20 19:32:36 +0300

    #5 New init_database.sql
    Maybe works, maybe not)

--- sql/init_database.sql
+++ sql/init_database.sql
@@ -1,8 +1,18 @@
-CREATE DATABASE "ServerNews"
+/*CREATE DATABASE "ServerNews"
     WITH 
     OWNER = postgres
     ENCODING = 'UTF8'
     LC_COLLATE = 'Russian_Russia.1251'
     LC_CTYPE = 'Russian_Russia.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+    */
+
+CREATE DATABASE "ServerNews"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'ru-RU.UTF-8'
+    LC_CTYPE = 'ru-RU.UTF-8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
