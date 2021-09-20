312e5899c09f49d63e70366d9e556458c6302b1a
paffel2 <61455510+paffel2@users.noreply.github.com>
2021-09-20 21:14:03 +0300

    small changes

--- sql/init_database.sql
+++ sql/init_database.sql
@@ -12,7 +12,7 @@ CREATE DATABASE "ServerNews"
     WITH 
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'ru-RU.UTF-8'
-    LC_CTYPE = 'ru-RU.UTF-8'
+    LC_COLLATE = 'ru_RU.UTF-8'
+    LC_CTYPE = 'ru_RU.UTF-8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
