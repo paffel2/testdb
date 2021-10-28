bd98c507a7ddc45f203e7e8f460114d4dc95e167
Pavel Zarubin <paffel2@gmail.com>
2021-10-28 10:43:45 +0300

    some fixes

--- sh/delete_author.sh
+++ sh/delete_author.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl  -X 'DELETE' -G "http://localhost:8000/delete_author?token=qwerty1" -F 'author_login=cybelin'
+curl  -X 'DELETE' -G "http://localhost:8000/authors/delete_author?token=qwerty1" -F 'author_login=cybelin'
